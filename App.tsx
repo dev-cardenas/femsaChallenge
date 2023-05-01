f35a9e263df0a5598569e6f79cb43c8848d50539
dev-cardenas <dev.cardenas@gmail.com>
2023-05-01 19:51:16 -0400

    remove error from FlatList

--- App.tsx
+++ App.tsx
@@ -3,8 +3,6 @@ import {StatusBar} from 'react-native';
 
 import {RootNavigator} from './src/navigators';
 import {Container} from './src/components';
-
-// import {SafeAreaView} from './src/components/SafeAreaView';
 import {AppColors} from './src/utils/index';
 
 function App(): JSX.Element {
